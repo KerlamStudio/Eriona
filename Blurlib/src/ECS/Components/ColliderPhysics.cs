815d44ca482bf93264edce99a3e63a0cb86d1d62
Maybeking <maybeking@protonmail.com>
2017-06-03 23:35:12 +0200

    A new type of collider where the position is uptaded depends on his velocity and multiple forces

---
+++ Blurlib/src/ECS/Components/ColliderPhysics.cs
@@ -0,0 +1,34 @@
+﻿using Blurlib.Physics;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blurlib.ECS.Components
+{
+    public class ColliderPhysics : Collider
+    {
+        public Vector2 Velocity;
+
+        public ForceList Forces { get; internal set; }
+
+        public Vector2 Acceleration { get { return Forces.Acceleration; } }
+
+        public float Friction;
+
+        public float Mass;
+
+        public ColliderPhysics(Transform transform, float mass, ForceList forces = null, float friction = 0.1f, bool collidable=true) : base(transform, collidable)
+        {
+            Mass = mass;
+
+            Forces = forces ?? new ForceList(Mass);
+
+            Friction = friction;
+
+            Velocity = Vector2.Zero;
+        }
+    }
+}
